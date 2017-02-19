da300cf41ca761e978245ff0437b253fa7fdfb31
Joan Merelos <joan@egissys.com>
2017-02-19 00:42:33 +0000

    add image uploader

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# FIX: NameError: uninitialized constant Recipe::PictureUploader
+require 'carrierwave/orm/activerecord'
